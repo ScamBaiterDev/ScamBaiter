9ba2a10a982bcb181034e23653fc014d299902bb
Chris Chrome <christophercookman@gmail.com>
2022-01-16 17:31:55 -0500

    ITS ALIVE!
    Co-authored-by: TheArcaneBrony <myrainbowdash949@gmail.com>

---
+++ index.js
@@ -0,0 +1,46 @@
+const Discord = require("discord.js");
+const axios = require("axios").default;
+const fs = require("fs");
+const config = require("./config.json");
+const bot = new Discord.Client({
+	intents: ["GUILD_MESSAGES", "GUILD_BANS", "GUILD_MEMBERS", "GUILD_INVITES", "GUILDS"]
+});
+
+var db = require("./db.json");
+
+bot.on('ready', () => {
+	console.log(`Logged in as ${bot.user.tag}`);
+	bot.user.setPresence(config.discord.status)
+	updateDb();
+    setInterval(updateDb, 1000*30*60);
+});
+
+
+bot.on("messageCreate", (msg) => {
+	if(db.some(link => msg.content.includes(link))) {
+		console.log("fuck there goes another scammy boi");
+        msg.delete().catch(() => {});
+        if(msg.member.bannable) {
+			msg.member.ban( { "reason": "AntiScam - Softban"} ).then((mem) => {
+				mem.guild.bans.remove(mem.user, "AntiScam - Softban");
+			}).catch(() => {});
+		}
+	}
+});
+
+bot.login(config.discord.token);
+
+
+function updateDb() {
+	//get
+	axios.get('https://phish.sinking.yachts/v2/all', {
+		headers: {
+			'User-Agent': 'ScamBaiter/1.0; Chris Chrome#9158'
+			// Mozilla/5.0 (compatible; <botname>/<botversion>; +<boturl>)
+		}
+	}).then((resp) => {
+        fs.writeFileSync('./db.json', JSON.stringify(resp.data));
+        console.log(`Updated db!`);
+		db = resp.data;
+    });
+}
