77c88cc91aeedb518a72a4989fed39eb8cbb49b8
Chris Chrome <christophercookman@gmail.com>
2022-01-16 19:09:12 -0500

    Add status command for operators

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const startup = new Date();
 const Discord = require("discord.js");
 const axios = require("axios").default;
 const fs = require("fs");
@@ -5,6 +6,7 @@ const config = require("./config.json");
 const bot = new Discord.Client({
 	intents: ["GUILD_MESSAGES", "GUILD_BANS", "GUILD_MEMBERS", "GUILD_INVITES", "GUILDS"]
 });
+
 
 var db = {};
 
@@ -21,12 +23,46 @@ bot.on("messageCreate", (msg) => {
 	if(db.some(link => msg.content.includes(link))) {
 		console.log("fuck there goes another scammy boi");
         msg.delete().catch(() => {});
-        if(msg.member.bannable && msg.member.permissions.has("KICK_MEMBERS")) {
+        if(msg.member.bannable && !msg.member.permissions.has("KICK_MEMBERS")) {
 			msg.author.send(config.discord.banMsg).finally(() => {
 				msg.member.ban( { "reason": "AntiScam - Softban"} ).then((mem) => {
 					mem.guild.bans.remove(mem.user, "AntiScam - Softban");
-				}).catch(() => {});
-			}).catch(() => {});
+				}).catch((err) => {console.error(err)});
+			}).catch((err) => {console.error(err)});
+		}
+	}
+	// Funky debug commands
+	const prefix = "$";
+    const args = msg.content.slice(prefix.length).trim().split(/ +/g);
+    const cmd = args.shift().toLowerCase();
+    if (msg.content.toLowerCase().startsWith(prefix)) {
+		switch(cmd) {
+			case "botinfo":
+				if(!config.owners.includes(msg.author.id)) return;
+				msg.channel.send({
+					embeds: [
+						{
+							"fields": [
+								{
+									"inline": true,
+									"name": "Guilds",
+									"value": bot.guilds.cache.size.toString()
+								},
+								{
+									"inline": true,
+									"name": "Current DB size",
+									"value": db.length.toString()
+								},
+								{
+									"inline": true,
+									"name": "Startup Time",
+									"value": `<t:${Math.floor(startup.getTime()/1000)}:D> <t:${Math.floor(startup.getTime()/1000)}:T>`
+								}
+							]
+						}
+					]
+				})
+			break;
 		}
 	}
 });
