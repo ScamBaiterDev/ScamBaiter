37db5ea6ed76fd823751ce39e759d19c8373ca38
Chris Chrome <christophercookman@gmail.com>
2022-01-17 15:06:22 -0500

    Hopefully fix that thing where it killed discordapp.com lol
    oh, also formatted it
    
    Co-authored-by: TheArcaneBrony <myrainbowdash949@gmail.com>

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const startup = new Date();
+const url = require("url");
 const Discord = require("discord.js");
 const axios = require("axios").default;
 const fs = require("fs");
@@ -14,64 +15,79 @@ bot.on('ready', () => {
 	console.log(`Logged in as ${bot.user.tag}`);
 	bot.user.setPresence(config.discord.status)
 	updateDb();
-    setInterval(updateDb, 1000*30*60);
+	setInterval(updateDb, 1000 * 30 * 60);
 });
 
 
 bot.on("messageCreate", (msg) => {
-	if(msg.author.bot) return;
-	if(db.some(link => msg.content.includes(link))) {
-		console.log("fuck there goes another scammy boi");
-        msg.delete().catch(() => {});
-        if(msg.member.bannable && !msg.member.permissions.has("KICK_MEMBERS")) {
-			msg.author.send(config.discord.banMsg).finally(() => {
-				msg.member.ban( { "reason": "AntiScam - Softban", "days": 1} ).then((mem) => {
-					mem.guild.bans.remove(mem.user, "AntiScam - Softban");
-				}).catch((err) => {console.error(err)});
-			}).catch((err) => {console.error(err)});
+	if (msg.author.bot) return;
+	db.forEach(x => {
+		if (msg.content.includes("https://" + x) || msg.content.includes("http://" + x)) {
+			// console.log(x);
+			msg.content.replace('\n', ' ').split(' ').forEach(x => {
+				// console.log(url.parse(x).hostname)
+				if (url.parse(x).hostname && db.includes(url.parse(x).hostname)) {
+					console.log(url.parse(x).hostname);
+					console.log("fuck there goes another scammy boi");
+					msg.delete().catch(() => {});
+					if (msg.member.bannable && !msg.member.permissions.has("KICK_MEMBERS")) {
+						msg.author.send(config.discord.banMsg).finally(() => {
+							msg.member.ban({
+								"reason": "AntiScam - Softban",
+								"days": 1
+							}).then((mem) => {
+								mem.guild.bans.remove(mem.user, "AntiScam - Softban");
+							}).catch((err) => {
+								console.error(err)
+							});
+						}).catch((err) => {
+							console.error(err)
+						});
+					}
+				}
+			});
 		}
-	}
+	})
 	// Funky debug commands
 	const prefix = "$";
-    const args = msg.content.slice(prefix.length).trim().split(/ +/g);
-    const cmd = args.shift().toLowerCase();
-    if (msg.content.toLowerCase().startsWith(prefix)) {
-		switch(cmd) {
+	const args = msg.content.slice(prefix.length).trim().split(/ +/g);
+	const cmd = args.shift().toLowerCase();
+	if (msg.content.toLowerCase().startsWith(prefix)) {
+		switch (cmd) {
 			case "botinfo":
-				if(!config.owners.includes(msg.author.id)) return;
+				if (!config.owners.includes(msg.author.id)) return;
 				msg.channel.send({
-					embeds: [
-						{
-							"title": "Bot Info",
-							"footer": {
-								"text": "This message will self destruct in 10 seconds"
+					embeds: [{
+						"title": "Bot Info",
+						"footer": {
+							"text": "This message will self destruct in 10 seconds"
+						},
+						"fields": [{
+								"inline": true,
+								"name": "Guilds",
+								"value": bot.guilds.cache.size.toString()
 							},
-							"fields": [
-								{
-									"inline": true,
-									"name": "Guilds",
-									"value": bot.guilds.cache.size.toString()
-								},
-								{
-									"inline": true,
-									"name": "Current DB size",
-									"value": db.length.toString()
-								},
-								{
-									"inline": true,
-									"name": "Startup Time",
-									"value": `<t:${Math.floor(startup.getTime()/1000)}:D> <t:${Math.floor(startup.getTime()/1000)}:T>`
-								}
-							]
-						}
-					]
+							{
+								"inline": true,
+								"name": "Current DB size",
+								"value": db.length.toString()
+							},
+							{
+								"inline": true,
+								"name": "Startup Time",
+								"value": `<t:${Math.floor(startup.getTime()/1000)}:D> <t:${Math.floor(startup.getTime()/1000)}:T>`
+							}
+						]
+					}]
 				}).then((msg1) => {
 					setTimeout(() => {
 						msg.delete();
 						msg1.delete();
 					}, 10000)
-				}).catch((err) => { console.log(err) });
-			break;
+				}).catch((err) => {
+					console.log(err)
+				});
+				break;
 		}
 	}
 });
@@ -87,11 +103,11 @@ function updateDb() {
 			// Mozilla/5.0 (compatible; <botname>/<botversion>; +<boturl>)
 		}
 	}).then((resp) => {
-        fs.writeFileSync('./db.json', JSON.stringify(resp.data));
-        console.log(`Updated db!`);
+		fs.writeFileSync('./db.json', JSON.stringify(resp.data));
+		console.log(`Updated db!`);
 		db = resp.data;
-    }).catch(() => {
+	}).catch(() => {
 		console.log("Fetching db failed, pls fix! REEEEEEEEEEEEEEEEEEEEEEEE!");
-        db = require("./db.json");
+		db = require("./db.json");
 	});
 }
