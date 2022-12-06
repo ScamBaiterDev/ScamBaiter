3e27371a17ba0414ed1debf04f7d20e40052f444
Scott Gould <greysilly7@gmail.com>
2022-12-05 19:40:41 -0500

    reduce size of messageCreate handler

--- src/helpers/checkers.ts
+++ src/helpers/checkers.ts
@@ -1,8 +1,10 @@
-import { urlRegex } from ".";
-import { scamDB } from "../bot";
-import { Message } from 'discord.js'
+import { DiscordInviteLinkRegex, urlRegex } from ".";
+import { scamDB, serverDB } from "../bot";
+import { Client, EmbedBuilder, Message, WebhookClient } from 'discord.js'
 import Jimp from "jimp";
 import jsQR from "jsqr";
+import { serverDBData } from '../types';
+import * as config from '../../config.json';
 
 export const checkForScamLinks = (urls: string): string[] => {
   const matches = urls.match(urlRegex);
@@ -22,10 +24,10 @@ export const checkForScamLinks = (urls: string): string[] => {
 
 export const checkAttachments = async (message: Message) => {
   message.attachments.forEach(async (attachment) => {
-    if (!attachment.contentType?.includes('image')) return;
+    if (!attachment.contentType?.includes('image')) return false;
     const image = await Jimp.read(attachment.url);
     const code = jsQR(image.bitmap.data as any, image.bitmap.width, image.bitmap.height);
-    if (code === null) return;
+    if (code === null) return false;
     if (code.data.startsWith('https://discord.com/ra/') || code.data.startsWith('https://discordapp.com/ra/')) {
       message.reply({
         'embeds': [{
@@ -36,4 +38,82 @@ export const checkAttachments = async (message: Message) => {
       });
     }
   });
+  return true;
+};
+
+export const checkForScamInvites = async (client: Client, content: string): Promise<{ invite?: string | undefined; badInvites?: serverDBData | undefined; }> => {
+  const inviteMatches = content.match(DiscordInviteLinkRegex);
+  if (inviteMatches === null) return {
+    invite: undefined,
+    badInvites: undefined
+  };
+  const inviteCode = inviteMatches.groups?.code;
+  const serverID = await client.fetchInvite(inviteCode ?? '').then((invite) => invite.guild?.id);
+
+  const badInvites = serverDB.filter(server => server.serverID === serverID);
+  if (badInvites.length === 0) return {
+    invite: undefined,
+    badInvites: undefined
+  };
+  return {
+    invite: inviteCode,
+    badInvites
+  };
+}
+
+const reportHook = new WebhookClient({ url: config.discord.reportHook })
+
+export const checkMessageContent = async (message: Message, content: string) => {
+  const scamLinks = checkForScamLinks(content);
+  const scamInvites = await checkForScamInvites(message.client, content);
+  if (scamLinks.length > 0 || scamInvites.badInvites !== undefined) {
+    if (message.deletable) message.delete();
+
+    const embed = new EmbedBuilder()
+      .setAuthor({
+        name: message.guild?.name ?? '',
+        iconURL: message.guild?.iconURL() ?? ''
+      })
+      .setThumbnail(message.author.avatarURL())
+      .setFooter({
+        text: `${message.id}${message.member?.bannable &&
+          !message.member.permissions.has('KickMembers')
+          ? ' | Softbanned'
+          : ' | Not Softbanned'
+          }`
+      })
+      .setFields(
+        {
+          name: 'User',
+          value: `${message.author} (${message.author.tag})\nID: ${message.author.id}`,
+        },
+        {
+          name: 'Message',
+          value: message.content,
+        },
+        {
+          name: scamInvites.badInvites !== undefined ? 'Invite' : 'Links',
+          value: scamInvites.badInvites !== undefined ? `https://discord.gg/${scamInvites.invite}` : scamLinks.join('\n'),
+        }
+      )
+      .setTimestamp(new Date());
+
+    await reportHook.send({ embeds: [embed] });
+
+    if (
+      message.member?.bannable &&
+      !message.member.permissions.has('KickMembers')
+    ) {
+      await message.author.send(
+        config.discord.banMsg.replace('{guild}', message.guild?.name ?? '')
+      );
+      await message.member.ban({
+        reason: 'Scam detected',
+        deleteMessageDays: 1
+      });
+      await message.guild?.bans.remove(message.author.id, 'AntiScam - Softban');
+    }
+  }
+
+  return true;
 };
