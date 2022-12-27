e5e1ac987989eff8ae01f7c90d949394fd3585f0
Scott Gould <greysilly7@gmail.com>
2022-12-27 11:23:08 -0500

    Dont die on checking attachments

--- src/helpers/checkers.ts
+++ src/helpers/checkers.ts
@@ -23,24 +23,29 @@ export const checkForScamLinks = (urls: string): string[] => {
 };
 
 export const checkAttachments = async (message: Message) => {
-  message.attachments.forEach(async (attachment) => {
-    if (!attachment.contentType?.includes('image')) return false;
-    const image = await Jimp.read(attachment.url);
-    const code = jsQR(image.bitmap.data as any, image.bitmap.width, image.bitmap.height);
-    if (code === null) return false;
-    if (code.data.startsWith('https://discord.com/ra/') || code.data.startsWith('https://discordapp.com/ra/')) {
-      message.reply({
-        'embeds': [{
-          'description': ':warning: POSSIBLE SCAM DETECTED :warning:\n\nThe image above contains a Discord Login QR code.\nScanning this code with the Discord app will give whoever made the code FULL ACCESS to your account',
-        }]
-      }).catch((err) => {
-        if (err && message.deletable) message.delete();
-      });
-      return true
-    }
+  try {
+    message.attachments.forEach(async (attachment) => {
+      if (!attachment.contentType?.includes('image')) return false;
+      const image = await Jimp.read(attachment.url);
+      const code = jsQR(image.bitmap.data as any, image.bitmap.width, image.bitmap.height);
+      if (code === null) return false;
+      if (code.data.startsWith('https://discord.com/ra/') || code.data.startsWith('https://discordapp.com/ra/')) {
+        message.reply({
+          'embeds': [{
+            'description': ':warning: POSSIBLE SCAM DETECTED :warning:\n\nThe image above contains a Discord Login QR code.\nScanning this code with the Discord app will give whoever made the code FULL ACCESS to your account',
+          }]
+        }).catch((err) => {
+          if (err && message.deletable) message.delete();
+        });
+        return true
+      }
+    });
 
-  });
-  return false;
+    return false;
+  } catch (err) {
+    console.error("Error while checking attachments: ", err);
+    return false
+  }
 };
 
 export const checkForScamInvites = async (client: Client, content: string): Promise<{ invite?: string | undefined; badInvites?: serverDBData | undefined; }> => {
