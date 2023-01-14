20efd53fcd24493e8ac8e3aa456fad300411c8e2
Scott Gould <greysilly7@gmail.com>
2023-01-14 15:19:34 -0500

    Refractor checkForScamInvites

--- src/helpers/checkers.ts
+++ src/helpers/checkers.ts
@@ -50,24 +50,33 @@ export const checkAttachments = async (message: Message) => {
 };
 
 
-export const checkForScamInvites = async (client: Client, content: string): Promise<{ invite?: string | undefined; badInvites?: serverDBData | undefined; }> => {
+export const checkForScamInvites = async (client: Client, content: string): Promise<{ invite?: string; badInvites?: serverDBData }> => {
+  const inviteCode = extractInviteCode(content);
+  if (!inviteCode) return { invite: undefined, badInvites: undefined };
+
+  const serverID = await fetchServerID(client, inviteCode);
+  if (!serverID) return { invite: undefined, badInvites: undefined };
+
+  const badInvites = filterBadInvites(serverID);
+  if (!badInvites) return { invite: undefined, badInvites: undefined };
+
+  return { invite: inviteCode, badInvites };
+};
+
+const extractInviteCode = (content: string): string | undefined => {
   const inviteMatches = content.match(DiscordInviteLinkRegex);
-  if (inviteMatches === null) return {
-    invite: undefined,
-    badInvites: undefined
-  };
-  const inviteCode = inviteMatches[5];
-  const serverID = await client.fetchInvite(inviteCode ?? '').then((invite) => invite.guild?.id);
-  const badInvites = serverDB.filter(server => serverID === server.serverID);
-  if (badInvites.length === 0) return {
-    invite: undefined,
-    badInvites: undefined
-  };
-  return {
-    invite: inviteCode,
-    badInvites
-  };
+  return inviteMatches?.[5];
 }
+
+const fetchServerID = async (client: Client, inviteCode: string): Promise<string | undefined> => {
+  return client.fetchInvite(inviteCode)
+    .then((invite) => invite.guild?.id);
+};
+
+const filterBadInvites = (serverID: string): serverDBData | undefined => {
+  return serverDB.filter(server => serverID === server.serverID);
+};
+
 
 const reportHook = new WebhookClient({ url: config.discord.reportHook })
 
