2e0cf12c07e165b97d8f07e38974bf7389351806
Scott Gould <greysilly7@gmail.com>
2023-01-16 20:17:17 -0500

    Commands shouldn't break but I am suspicious of checkers

--- src/index.ts
+++ src/index.ts
@@ -83,10 +83,14 @@ bot.on('messageCreate', async message => {
   const args = message.content.slice(prefix.length).trim().split(/ +/g);
   const cmd = args.shift()?.toLowerCase();
 
-  if (cmd !== 'check') {
-    const contentCheckResult = await checkMessageContent(message);
-    const attachmentCheckResult = await checkAttachments(message);
-    if (contentCheckResult || attachmentCheckResult) return;
+  try {
+    if (cmd !== 'check') {
+      const contentCheckResult = await checkMessageContent(message);
+      const attachmentCheckResult = await checkAttachments(message);
+      if (contentCheckResult || attachmentCheckResult) return;
+    }
+  } catch (err) {
+    console.error(`Listen Checkers is broke idk why but heres the error: ${err}`);
   }
 
   // Check if command exists
